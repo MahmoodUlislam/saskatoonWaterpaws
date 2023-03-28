6efcc96b31f15728c60715b962c5ce9a1afbb11d
MahmoodULIslam <86877369+MahmoodUlislam@users.noreply.github.com>
2023-03-27 23:50:30 -0600

    update 27.3.2023

--- wp-content/plugins/content-slider-block/dist/editor.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return [
-	'dependencies' => [ 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-rich-text', 'jquery', 'jquery-ui-droppable', 'swiperJS' ],
-	'version' => CSB_PLUGIN_VERSION
-];
--- wp-content/plugins/content-slider-block/dist/script.asset.php
+++
@@ -1,4 +0,0 @@
-<?php return [
-	'dependencies' => [ 'wp-element', 'jquery', 'swiperJS' ],
-	'version' => CSB_PLUGIN_VERSION
-];
